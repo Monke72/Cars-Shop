40a56fa09f67bf792dab0cf64ed25c9498342271
Monke72 <wera16307@gmail.com>
2025-04-28 20:05:56 +0400

    start

---
+++ shop/src/getCarsFunc.tsx
@@ -0,0 +1,23 @@
+export interface ICars {
+  name: string;
+  description: string;
+  price: number;
+  acceleration: number;
+  urlPhoto: string;
+}
+
+async function getCars(
+  newUrl = "",
+  setState: React.Dispatch<React.SetStateAction<ICars[]>>
+) {
+  const url = "https://6790b4e0af8442fd73775266.mockapi.io/cars";
+  const fetchUrl = url + newUrl;
+  try {
+    const res = await fetch(fetchUrl);
+    const data = await res.json();
+    setState(data);
+  } catch (e) {
+    alert(e);
+  }
+}
+export default getCars;
